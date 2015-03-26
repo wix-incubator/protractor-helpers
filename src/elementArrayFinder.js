7270c3fb52ece4905ec9f7c9ab5f7308ffc9ed8f
Alon Yehezkel <alony@wix.com>
2015-03-26 14:14:45 +0200

    add getWebElement before getText

--- src/elementArrayFinder.js
+++ src/elementArrayFinder.js
@@ -5,7 +5,7 @@ var ElementArrayFinder = $$('').constructor;
 ElementArrayFinder.prototype.getByText = function (compareText) {
 	var foundElement;
 	return this.each(function (element) {
-		element.getText().then(function (elementText) {
+		element.getWebElement().getText().then(function (elementText) {
 			if (elementText.trim() === compareText) {
 				foundElement = element;
 			}
