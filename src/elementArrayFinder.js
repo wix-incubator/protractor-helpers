27a0451e2c27d8f43f9e7c361e9ebdac853c6cf0
Raziel Alron <razielron@gmail.com>
2017-03-04 00:03:16 +0200

    Fix issue #37
    Change the returned value.
    Now the functions: 'getByText', 'getByAttribute' returns an element so you can 'click' him.

--- src/elementArrayFinder.js
+++ src/elementArrayFinder.js
@@ -5,18 +5,18 @@ var ElementFinder = $('').constructor;
 
 ElementArrayFinder.prototype.getByText = function (compareText) {
     return this.filter(function (element) {
-        element.getWebElement().getText().then(function (elementText) {
+        return element.getWebElement().getText().then(function (elementText) {
             return elementText.trim() === compareText;
         })
-    });
+    }).first();
 };
 
 ElementArrayFinder.prototype.getByAttribute = function (attribute, value) {
     return this.filter(function (element) {
-        element.getWebElement().getAttribute(attribute).then(function (elementAttribute) {
+        return element.getWebElement().getAttribute(attribute).then(function (elementAttribute) {
             return elementAttribute === value;
         })
-    });
+    }).first();
 };
 
 ElementArrayFinder.prototype.$$data = ElementFinder.prototype.$$data = function (hook) {
