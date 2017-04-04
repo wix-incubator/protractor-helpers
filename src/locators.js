01a22ce86eee9feaa07684349fbf64ed64cbf24b
Yaniv Efraim <yaniv.efraim@gmail.com>
2017-04-04 18:15:57 +0300

    add support for protractor 5

--- src/locators.js
+++ src/locators.js
@@ -5,12 +5,12 @@ var MULTI_LOCATOR = 'dataHookAll';
 // Data-hook is a unique attribute to find elements in e2e tests.
 (function () {
     by.addLocator(SINGLE_LOCATOR, function (hook, optParentElement, optRootSelector) {
-        var using = optParentElement || document.querySelector(optRootSelector) || document;
+        var using = optParentElement || (optRootSelector && document.querySelector(optRootSelector)) || document;
         return using.querySelector('[data-hook=\'' + hook + '\']');
     });
 
     by.addLocator(MULTI_LOCATOR, function (hook, optParentElement, optRootSelector) {
-        var using = optParentElement || document.querySelector(optRootSelector) || document;
+        var using = optParentElement || (optRootSelector && document.querySelector(optRootSelector)) || document;
         return using.querySelectorAll('[data-hook=\'' + hook + '\']');
     });
 })();
